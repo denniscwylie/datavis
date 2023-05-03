adfc72a30132aeff5bdf95d5b39f2860048cef3a
Dennis Wylie <denniscwylie@gmail.com>
2023-05-03 13:58:26 -0500

    fix bug in python version handling of annot

--- datavis.py
+++ datavis.py
@@ -20,7 +20,7 @@ def rt(f, sep='\t', index_col=0, header=0, *args, **kwargs):
 logTpm = rt('gse75386_logtpm_filtered.tsv.gz')
 trxAnnot = rt('Mus_musculus_GRCm38_82_TranscriptMap.tsv.gz')
 trxAnnot = trxAnnot.reindex(logTpm.index)
-annot = rt('gse75386_processed_annot.tsv')
+annot = rt('gse75386_processed_annot.tsv').loc[logTpm.columns]
 annot.head()
 
 cellType = annot['type']
